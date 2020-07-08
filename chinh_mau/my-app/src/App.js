eef86e1af5da92fb844aa59cfdf3fd349554e043
kiemsibat <hoanglan1989vn@gmail.com>
2020-07-08 16:32:10 +0700

    first commit

---
+++ chinh_mau/my-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+      
+    <div className="navbar">
+    <a className="navbar-brand" href="#">Title</a>
+    <ul className="nav navbar-nav">
+      <li className="active">
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Link</a>
+      </li>
+    </ul>
+  </div>
+  
+      
+   
+  );
+}
+
+export default App;
